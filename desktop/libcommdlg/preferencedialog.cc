6279d07155fbb7d1f83b5d675e13082ddc908d5b
cat <cat@originull.org>
2022-12-09 18:27:03 -0800

    libcommdlg: add source

--- desktop/libcommdlg/preferencedialog.cc
+++ desktop/libcommdlg/preferencedialog.cc
@@ -1,50 +1,55 @@
 #include "preferencedialog.h"
-#include <QToolBar>
-#include <QAction>
+#include "preferencedialog_p.h"
 #include <QVBoxLayout>
 #include <QApplication>
-#include <QDialog>
 #include <QPropertyAnimation>
 
-ADPreferenceDialog::ADPreferenceDialog(QWidget *parent)
-    :QDialog(parent),
-     m_tool(new QToolBar(this)),
-     m_placeholder(new QWidget(this)),
-     m_current(m_placeholder)
+HWPreferenceDialogPrivate::HWPreferenceDialogPrivate(HWPreferenceDialog *parent)
+    : d(parent)
+    , m_tool(new QToolBar(parent))
+    , m_placeholder(new QWidget(parent))
+    , m_current(m_placeholder)
+{
+
+}
+
+HWPreferenceDialog::HWPreferenceDialog(QWidget *parent)
+    : QDialog(parent)
+    , p(new HWPreferenceDialogPrivate(this))
 {
     setWindowTitle(tr("Preferences"));
     resize(400,40);
-    m_tool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    p->m_tool->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
     auto layout = new QVBoxLayout(this);
     layout->setContentsMargins(0,0,0,0);
     setLayout(layout);
 
-    layout->addWidget(m_tool);
-    layout->addWidget(m_placeholder, 1);
-    auto lspacer = new QWidget(m_tool);
-    auto rspacer = new QWidget(m_tool);
+    layout->addWidget(p->m_tool);
+    layout->addWidget(p->m_placeholder, 1);
+    auto lspacer = new QWidget(p->m_tool);
+    auto rspacer = new QWidget(p->m_tool);
 
-    auto p = lspacer->palette();
-    p.setColor(QPalette::All, QPalette::Base, QColor(0,0,0,0));
-    lspacer->setPalette(p);
+    auto pal = lspacer->palette();
+    pal.setColor(QPalette::All, QPalette::Base, QColor(0,0,0,0));
+    lspacer->setPalette(pal);
     lspacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
     lspacer->setMaximumHeight(3);
-    rspacer->setPalette(p);
+    rspacer->setPalette(pal);
     rspacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
     rspacer->setMaximumHeight(3);
 
-    m_leftSpacerAct = m_tool->addWidget(lspacer);
-    m_rightSpacerAct = m_tool->addWidget(rspacer);
+    p->m_leftSpacerAct = p->m_tool->addWidget(lspacer);
+    p->m_rightSpacerAct = p->m_tool->addWidget(rspacer);
 
 }
 
-ADPreferenceDialog::~ADPreferenceDialog()
+HWPreferenceDialog::~HWPreferenceDialog()
 {
-    for(auto w : m_tabcontent)
+    for(auto w : p->m_tabcontent)
         w->deleteLater();
 }
 
-void ADPreferenceDialog::addPage(QWidget *page, const QIcon &icon, const QString &title)
+void HWPreferenceDialog::addPage(QWidget *page, const QIcon &icon, const QString &title)
 {
     bool rtl = false;
     if(qApp->layoutDirection() == Qt::RightToLeft)
@@ -56,50 +61,50 @@ void ADPreferenceDialog::addPage(QWidget *page, const QIcon &icon, const QString
     else
         action = new QAction(icon, title, this);
 
-    m_tabs.append(action);
-    m_tabcontent.append(page);
+    p->m_tabs.append(action);
+    p->m_tabcontent.append(page);
     page->setVisible(false);
     if(rtl)
     {
-        if(m_tabs.count() == 0)
-            m_tool->insertAction(m_rightSpacerAct, action);
+        if(p->m_tabs.count() == 0)
+            p->m_tool->insertAction(p->m_rightSpacerAct, action);
         else
-            m_tool->insertAction(m_tabs.last(), action);
+            p->m_tool->insertAction(p->m_tabs.last(), action);
     }
     else
-        m_tool->insertAction(m_rightSpacerAct, action);
+        p->m_tool->insertAction(p->m_rightSpacerAct, action);
 
     action->setCheckable(true);
-    connect(action, &QAction::triggered, this, &ADPreferenceDialog::tabActionTriggered);
+    connect(action, &QAction::triggered, this, &HWPreferenceDialog::tabActionTriggered);
 }
 
-void ADPreferenceDialog::setAnimated(bool animated)
+void HWPreferenceDialog::setAnimated(bool animated)
 {
-    m_animate = animated;
+    p->m_animate = animated;
     emit animatedChanged();
 }
 
-void ADPreferenceDialog::setCurrentIndex(int index)
+void HWPreferenceDialog::setCurrentIndex(int index)
 {
-    if(index > m_tabs.count()-1)
+    if(index > p->m_tabs.count()-1)
         return;
 
     if(index < 0)
         return;
 
-    for(auto action : m_tabs)
+    for(auto action : p->m_tabs)
         action->setChecked(false);
 
-    for(auto view : m_tabcontent)
+    for(auto view : p->m_tabcontent)
         view->setVisible(false);
 
-    m_tabcontent[index]->setVisible(true);
-    if(m_animate)
+    p->m_tabcontent[index]->setVisible(true);
+    if(p->m_animate)
     {
         auto animation = new QPropertyAnimation(this, "size");
         QSize start = size();
-        auto wheight = m_tabcontent[index]->minimumHeight();
-        QSize end = QSize(width(), wheight+m_tool->height());
+        auto wheight = p->m_tabcontent[index]->minimumHeight();
+        QSize end = QSize(width(), wheight+p->m_tool->height());
         animation->setStartValue(start);
         animation->setEndValue(end);
         animation->setDuration(190);
@@ -108,43 +113,44 @@ void ADPreferenceDialog::setCurrentIndex(int index)
     }
     else
     {
-        auto wheight = m_tabcontent[index]->heightForWidth(width());
-        QSize end = QSize(width(), wheight+m_tool->height());
+        auto wheight = p->m_tabcontent[index]->heightForWidth(width());
+        QSize end = QSize(width(), wheight+p->m_tool->height());
         resize(end);
     }
 
-    layout()->replaceWidget(m_current, m_tabcontent[index]);
-    m_current = m_tabcontent[index];
-    m_current->setVisible(true);
-    m_index = index;
-    m_tabs[index]->setChecked(true);
+    layout()->replaceWidget(p->m_current, p->m_tabcontent[index]);
+    p->m_current = p->m_tabcontent[index];
+    p->m_current->setVisible(true);
+    p->m_index = index;
+    p->m_tabs[index]->setChecked(true);
     emit currentIndexChanged(index);
 }
 
-void ADPreferenceDialog::show()
+void HWPreferenceDialog::show()
 {
-    if(m_tabs.count() < 1)
+    if(p->m_tabs.count() < 1)
         return;
 
     setCurrentIndex(0);
     QDialog::show();
 }
 
-bool ADPreferenceDialog::animated()
+bool HWPreferenceDialog::animated()
 {
-    return m_animate;
+    return p->m_animate;
 }
 
-int ADPreferenceDialog::currentIndex()
+int HWPreferenceDialog::currentIndex()
 {
-    return m_index;
+    return p->m_index;
 }
 
-void ADPreferenceDialog::tabActionTriggered()
+void HWPreferenceDialog::tabActionTriggered()
 {
     auto senderObj = qobject_cast<QAction*>(sender());
     Q_ASSERT(senderObj);
 
-    if(m_tabs.contains(senderObj))
-         setCurrentIndex(m_tabs.indexOf(senderObj));
+    if(p->m_tabs.contains(senderObj))
+         setCurrentIndex(p->m_tabs.indexOf(senderObj));
 }
+
