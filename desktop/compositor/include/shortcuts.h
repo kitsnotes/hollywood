fee48db9c3f45ca5290f728f6e8bf9976c35da09
cat <cat@originull.org>
2023-03-18 17:43:02 -0700

    compositor: improvements to shortcut handling class

--- desktop/compositor/include/shortcuts.h
+++ desktop/compositor/include/shortcuts.h
@@ -8,6 +8,15 @@
 #include <QObject>
 #include <QShortcut>
 #include <QKeySequence>
+#include <QHash>
+#include <utility>
+
+class KeyCombination {
+public:
+    QKeyCombination combo;
+    std::function<void()> signal = nullptr;
+    bool operator==(const KeyCombination &l) const;
+};
 
 class ShortcutManager : public QObject
 {
@@ -15,16 +24,48 @@ class ShortcutManager : public QObject
 public:
     explicit ShortcutManager(QObject *parent = nullptr);
     bool checkAndHandleCombo(const QKeyCombination &combo);
+
+
 signals:
-
-private slots:
-    void brightnessUp();
-    void brightnessDown();
+    void monitorBrightnessUp();
+    void monitorBrightnessDown();
+    void keyboardBrightnessUp();
+    void keyboardBrightnessDown();
+    void printScreenRequested();
+    void windowSwitchRequested();
+    void volumeDownRequested();
+    void volumeUpRequested();
+    void volumeMuteRequested();
+    void webBrowserRequested();
+    void searchRequested();
+public slots:
+    void reloadConfig();
 private:
-    QKeyCombination m_brightness_up;
-    QKeyCombination m_brightness_down;
+    // Customizable Keys
+    KeyCombination m_switch_windows;
+    KeyCombination m_full_screenshot;
 
-    QList<QKeyCombination> m_tracked;
+    KeyCombination m_mon_brightness_up;
+    KeyCombination m_mon_brightness_down;
+    KeyCombination m_kbd_brightness_up;
+    KeyCombination m_kbd_brightness_down;
+
+    // Multimedia Keyboard extras (always bound)
+    KeyCombination m_mon_brightness_up_media;
+    KeyCombination m_mon_brightness_down_media;
+    KeyCombination m_kbd_brightness_up_media;
+    KeyCombination m_kbd_brightness_down_media;
+
+    KeyCombination m_vol_up_media;
+    KeyCombination m_vol_down_media;
+    KeyCombination m_vol_mute_media;
+
+    KeyCombination m_launch_browser_media;
+    KeyCombination m_start_search_media;
+
+
+    // master list of global tracked combos
+    QHash<QKeyCombination, KeyCombination> m_tracked;
 };
 
 #endif // SHORTCUTMANAGER_H
